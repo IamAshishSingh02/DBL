02a41e84251c60844c4081d110fd71d8769b1d69
ASHISH KUMAR <145495068+IamAshishSingh02@users.noreply.github.com>
2025-09-15 23:59:52 +0530

    Update asgn5.sql

--- asgn5.sql
+++ asgn5.sql
@@ -1,65 +1,66 @@
 create database asgn5;
 use asgn5;
 
-create table stud_marks(
-    roll number(5) primary key,
-    name varchar2(30),
-    total_marks number(5)
+create table stud_marks (
+  roll int primary key,
+  name varchar(30),
+  total_marks int
 );
 
-create table result(
-    roll number(5),
-    name varchar2(30),
-    class varchar2(20)
+create table result (
+  roll int,
+  name varchar(30),
+  class varchar(30)
 );
 
--- procedure for grading
-create or replace procedure proc_grade(
-    p_roll in number,
-    p_name in varchar2,
-    p_marks in number
-) is
-    v_class varchar2(20);
+delimiter $$
+
+create procedure proc_grade(in p_roll int, in p_name varchar(30), in p_marks int)
 begin
+    declare v_class varchar(30);
+
+    declare exit handler for sqlexception
+    begin
+        select 'error occurred while processing proc_grade' as message;
+        rollback;
+    end;
+
     if p_marks between 990 and 1500 then
-        v_class := 'distinction';
-    elsif p_marks between 900 and 989 then
-        v_class := 'first class';
-    elsif p_marks between 825 and 899 then
-        v_class := 'higher second class';
+        set v_class = 'distinction';
+    elseif p_marks between 900 and 989 then
+        set v_class = 'first class';
+    elseif p_marks between 825 and 899 then
+        set v_class = 'higher second class';
     else
-        v_class := 'fail';
+        set v_class = 'fail';
     end if;
 
-    insert into result values(p_roll, p_name, v_class);
-end;
-/
+    insert into result (roll, name, class) values (p_roll, p_name, v_class);
+end$$
 
--- function for grading
-create or replace function func_grade(
-    p_marks in number
-) return varchar2 is
-    v_class varchar2(20);
+create function func_grade(p_marks int) returns varchar(30)
+deterministic
 begin
+    declare v_class varchar(30);
+
     if p_marks between 990 and 1500 then
-        v_class := 'distinction';
-    elsif p_marks between 900 and 989 then
-        v_class := 'first class';
-    elsif p_marks between 825 and 899 then
-        v_class := 'higher second class';
+        set v_class = 'distinction';
+    elseif p_marks between 900 and 989 then
+        set v_class = 'first class';
+    elseif p_marks between 825 and 899 then
+        set v_class = 'higher second class';
     else
-        v_class := 'fail';
+        set v_class = 'fail';
     end if;
+
     return v_class;
-end;
-/
+end$$
 
-begin
-    proc_grade(1, 'Ashish', 1200);
-    proc_grade(2, 'Ashu', 950);
-    proc_grade(3, 'Random1', 860);
-    proc_grade(4, 'Random2', 700);
-end;
-/
+delimiter ;
+
+call proc_grade(1, 'ashish', 1200);
+call proc_grade(2, 'ashu', 950);
+call proc_grade(3, 'random1', 860);
+call proc_grade(4, 'random2', 700);
 
 select * from result;
